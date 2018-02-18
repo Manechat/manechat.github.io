8048bf1a1d752df758096d1b7451b684539c76a9
Tawmy <kersting.tom@gmail.com>
2018-02-18 18:58:56 +0100

    Replace Twilight, remove Dash hiding until I think of a better solution

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ $(document).ready(function(){
 		$('.modal').addClass("modal-show");
 		$('section,header,nav,footer').addClass("blur");
 		$('.button').css({"pointer-events" : "none"});
-		$('body').css({"background" : "#36393e"});
 	});
 	$('section,header').mouseup(function(){
 		$('.modal').removeClass("modal-show");
 		$('section,header,nav,footer').removeClass("blur");
 		$('.button').css({"pointer-events" : "initial"});
-		$('body').css({"background" : "#36393e url('img/rdsalute_bg.png') fixed right bottom no-repeat"});
 	});
 });
