ff63846d81f1ec172839ab6968487fbb5f687142
Tawmy <kersting.tom@gmail.com>
2018-02-18 21:18:58 +0100

    Update "last updated" because I forgot to do that

--- script.js
+++ script.js
@@ -9,4 +9,13 @@ $(document).ready(function(){
 		$('section,header,nav,footer').removeClass("blur");
 		$('.button').css({"pointer-events" : "initial"});
 	});
+
+	$('#twismile').click(function(){
+		if(!($(this).hasClass("slide"))){
+			$('#twismile').addClass("slide");
+			setTimeout(function(){
+	            $('#twismile').removeClass("slide");
+	        }, 4000);
+		}
+	});
 });
