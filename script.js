7f84869e8130c5dc8cd01fbf815d9ddaa3b39016
Tawmy <kersting.tom@gmail.com>
2018-04-10 14:30:14 +0200

    Prevent scrolling when confirmation modal is open

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ $(document).ready(function(){
 		$('.modal').addClass("modal-show");
 		$('section,header,nav,footer').addClass("blur");
 		$('.button').css({"pointer-events" : "none"});
+		$('html,body').css({"overflow" : "hidden"});
 	});
 	$('section,header').mouseup(function(){
 		$('.modal').removeClass("modal-show");
 		$('section,header,nav,footer').removeClass("blur");
 		$('.button').css({"pointer-events" : "initial"});
+		$('html,body').css({"overflow" : "initial"});
 	});
 
 	$('#twismile').click(function(){
