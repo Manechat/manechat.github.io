e672132605e377205b331495096e9ec9da9cbb44
Tawmy <kersting.tom@gmail.com>
2018-12-30 21:21:40 +0100

    Maintenance

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ $(document).ready(function () {
 	$('#join-button').click(function () {
 		$('.modal').addClass("modal-show");
 		$('section,header,nav,footer').addClass("blur");
-		$('.button').css({ "pointer-events": "none" });
-		$('html,body').css({ "overflow": "hidden" });
+		$('.button, nav, section a').css({ "pointer-events": "none" });
+		$('html,body').css({ "overflow": "hidden"});
 	});
 	$('section,header').mouseup(function () {
 		$('.modal').removeClass("modal-show");
 		$('section,header,nav,footer').removeClass("blur");
-		$('.button').css({ "pointer-events": "initial" });
+		$('.button, nav, section a').css({ "pointer-events": "initial" });
 		$('html,body').css({ "overflow": "initial" });
 	});
 
